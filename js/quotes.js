1f6a96361b496f330418e9fad19192e60fb1e814
Ole Petter Baugerød Stokke <tantepose@users.noreply.github.com>
2017-09-07 22:38:02 +0200

    First upload of random quote generator project
    Project #1 of my full stack Javascript techdegree.

---
+++ js/quotes.js
@@ -0,0 +1,73 @@
+//array containg each quote as an object
+var quotes = [
+  {
+    quote: "Never tell me the odds.", //whats been said
+    source: "Han Solo", //who said it
+    citation: "Star Wars: Episode V – The Empire Strikes Back", //where it was said
+    year: 1980, //when it was said
+    link: "https://www.youtube.com/watch?v=uYX-NSZMqt0" //where to hear or read more
+  },
+  {
+    quote: "Do. Or do not. There is no try.",
+    source: "Yoda",
+    citation: "Star Wars: Episode V – The Empire Strikes Back",
+    year: 1980,
+    link: "https://www.youtube.com/watch?v=BQ4yd2W50No"
+  },
+  {
+    quote: "Fear leads to anger; anger leads to hate; hate leads to suffering.",
+    source: "Yoda",
+    citation: "Star Wars: Episode I – The Phantom Menace",
+    year: 1999,
+    link: "https://www.youtube.com/watch?v=91_G8iaokk8"
+  },
+  {
+    quote: "So this is how liberty dies. With thunderous applause.",
+    source: "Padmé Amidala",
+    citation: "Star Wars: Episode III – Revenge of the Sith",
+    year: 2005,
+    link: "https://www.youtube.com/watch?v=DgxZr6LLS34"
+  },
+  {
+    quote: "Hokey religions and ancient weapons are no match for a good blaster at your side, kid.",
+    source: "Han Solo",
+    citation: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+    link: "https://www.youtube.com/watch?v=V268Qk6-xsw"
+  },
+  {
+    quote: "I’m one with the Force. The Force is with me.",
+    source: "Chirrut Îmwe",
+    citation: "Rogue One: A Star Wars Story",
+    year: 2016,
+    link: "https://www.youtube.com/watch?v=UdV4BCO1w7Y"
+  },
+  {
+    quote: "We meet again, at last. The circle is now complete. When I left you, I was but the learner; now I am the master.",
+    source: "Darth Vader",
+    citation: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+    link: "https://www.youtube.com/watch?v=EZD1f9RTEJU"
+  },
+  {
+    quote: "Your eyes can deceive you. Don’t trust them.",
+    source: "Obi-Wan Kenobi",
+    //citation missing to demonstrate failsafe
+    year: 1977,
+    link: "https://www.youtube.com/watch?v=oDIrOE_fnl8"
+  },
+  {
+    quote: "Who’s the more foolish? The fool or the fool who follows him?",
+    source: "Obi-Wan Kenobi",
+    citation: "Star Wars: Episode IV - A New Hope",
+    //year missing to demonstrate failsafe
+    link: "https://www.youtube.com/watch?v=x0ow4X8tiMI"
+  },
+  {
+    quote: "In my experience there is no such thing as luck.",
+    source: "Obi-Wan Kenobi",
+    citation: "Star Wars: Episode IV – A New Hope",
+    year: 1977,
+    //link missing to demonstrate failsafe
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,85 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+var quotesLog = []; //array for storing whats been shown allready
+var timer = setInterval(printQuote, 20000); //getting a quote every 20th second
+
+window.onload = function (){
+  printQuote(); //start with a random quote
+}
+
+//get a random quote from the quotes array in quotes.js
+//and make sure it hasn't allready been shown
+function getRandomQuote (){
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  console.log("*** Getting quote | randomNumber = " + randomNumber + " ***");
+  console.log("-> quotes shown: " + quotesLog.length + " [" + quotesLog.join(",") + "]");
+
+  if (quotesLog.length < quotes.length){ //if unused quotes left, lets try
+
+    for (i = 0; i <= quotesLog.length; i++){ //is this quote allready shown?
+
+        if (quotesLog.indexOf(randomNumber) > -1){ //yes: try again with new number
+          randomNumber = Math.floor(Math.random() * quotes.length);
+          console.log("Quote shown, trying again with " + randomNumber);
+          i = -1; //resetting the loop
+
+        } else { //no: return the quote, push number to log
+            console.log("Quote NOT shown, returning: " + quotes[randomNumber].quote);
+            quotesLog.push(randomNumber);
+            return quotes[randomNumber];
+          }
+        }
+
+    } else { //no quotes left, returning quote and starting over
+        console.log("Quoteslog full, resetting and returning: " + quotes[randomNumber].quote);
+        quotesLog = []; //empty log
+        quotesLog.push(randomNumber);
+        return quotes[randomNumber];
+    }
+}
+
+//gathering and outputting the quote and any additional data
+function printQuote(){
+  changeBackground();
+  setTimer(); //reset timer, avoiding new quote earlier then 20 sec after click
+  var randomQuote = getRandomQuote(); //get random quote as object
+  var textToPrint;  //declaring the string to send to the HTML
+
+  //this prints every time (quote and source, required fields)
+  textToPrint = '<p class="quote">' + randomQuote.quote + '</p>' +
+    '<p class="source">' + randomQuote.source;
+
+  if (randomQuote.citation){ //do we have a citation? if so; print it
+    textToPrint += '<span class="citation">' + randomQuote.citation + '</span>'
+  }
+  if (randomQuote.year){ //do we have a year?
+    textToPrint += '<span class="year">' + randomQuote.year + '</span>';
+  }
+  if (randomQuote.link){ //do we have a link?
+    textToPrint += '<span class="link"><a target="_blank" href="' +
+    randomQuote.link + '"> &#128065</a></span>'; //unicode for an eye
+  }
+
+  textToPrint += '</p>'; //ending the paragraph "source"
+  document.getElementById('quote-box').innerHTML = textToPrint; //print it
+}
+
+//changing the background per quote
+function changeBackground(){
+  var letters = "0123456789ABCDEF"; //building bricks of hex color
+  var color = "#"; //declaring and starting the hex color to be made
+  for (var i = 0; i < 6; i++) { //hex color consists of six numbers/letters
+    color += letters.charAt(Math.floor(Math.random() * 16)); //mixing the 16 bricks
+  }
+  document.body.style.backgroundColor = color;
+  document.getElementById("loadQuote").style.backgroundColor = color;
+}
+
+//reset the timer by turning it off and on again
+function setTimer(){
+  console.log("Setting timer for 20 seconds.");
+  clearInterval(timer);
+  timer = setInterval(printQuote, 20000);
+}
